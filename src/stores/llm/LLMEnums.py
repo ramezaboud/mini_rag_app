069d61a0e4d81479d2e3c45fb2946944611e838d
ramezaboud <ramezaboud248@gmail.com>
2026-03-02 22:10:24 +0200

    Add CoHereProvider implementation and update LLMEnums for enhanced functionality

--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -10,4 +10,19 @@ class OpenAIEnums(Enum):
 
     SYSTEM = "system"
     USER = "user"
+    ASSISTANT = "CHATBOT"
+
+class CohereEnums(Enum):
+
+    SYSTEM = "system"
+    USER = "user"
     ASSISTANT = "assistant"
+
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
+
+
+class DocumentTypeEnum(Enum):
+
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
---
+++ src/stores/llm/providers/CoHereProvider.py
@@ -0,0 +1,104 @@
+from ..LLMInterface import LLMInterface
+from ..LLMEnums import CohereEnums, DocumentTypeEnum
+import cohere
+import logging
+
+class CoHereProvider(LLMInterface):
+
+    def __init__(self, api_key: str
+                     , default_input_max_characters: int = 1000
+                     , default_generation_max_output_tokens: int = 1000
+                     , default_generation_temperature: float = 0.1):
+        
+        self.api_key = api_key
+
+        self.default_input_max_characters = default_input_max_characters
+        self.default_generation_max_output_tokens = default_generation_max_output_tokens
+        self.default_generation_temperature = default_generation_temperature
+
+        self.generation_model_id = None
+
+        self.embedding_model_id = None
+        self.embedding_size = None
+
+        self.client = cohere.Client(
+            api_key = self.api_key
+            )
+
+        self.logger = logging.getLogger(__name__)
+
+
+
+    def set_generation_model(self, model_id: str):
+        self.generation_model_id = model_id
+
+    def set_embedding_model(self, model_id: str, embedding_size: int):
+        self.embedding_model_id = model_id
+        self.embedding_size = embedding_size
+
+    def process_text(self, text: str):
+        return text[:self.default_input_max_characters].strip()
+    
+
+
+    def generate_text(self, prompt: str, chat_history : list = [], max_output_tokens: int = None
+                          , temperature: float = None):
+        
+        if not self.client:
+            self.logger.error("Cohere client is not set.")
+
+        if not self.generation_model_id:
+            self.logger.error("Generation model ID for Cohere is not set.")
+
+        max_output_tokens = max_output_tokens if max_output_tokens else self.default_generation_max_output_tokens
+        temperature = temperature if temperature else self.default_generation_temperature
+
+        response = self.client.chat(
+            model = self.generation_model_id,
+            chat_history = chat_history ,
+            message = self.process_text(prompt),
+            temperature = temperature,
+            max_tokens = max_output_tokens
+        )
+
+        if not response or not response.text:
+            self.logger.error("Error while generating text with CoHere.")
+            return None
+        
+        return response.text
+        
+
+
+    def embed_text(self, text: str, document_id: str = None):
+
+        if not self.client:
+            self.logger.error("Cohere client is not set.")
+        
+        if not self.embedding_model_id:
+            self.logger.error("Embedding model ID for Cohere is not set.")
+
+        input_type = CohereEnums.DOCUMENT
+        if DocumentTypeEnum.QUERY:
+            input_type = CohereEnums.QUERY
+            
+        response = self.client.embed(
+            model = self.embedding_model_id,
+            input = self.process_text(text),
+            input_type = input_type,
+            mbedding_types=["float"]
+        )
+
+        if not response or not response.embeddings or not response.embeddings.float:
+            self.logger.error("Error while embedding text with CoHere.")
+            return None
+
+        return response.embeddings.float[0]
+
+
+    def construct_prompt(self, prompt: str, role: str):
+
+        return{
+            "role": role,
+            "content": self.process_text(prompt)
+        }
+    
