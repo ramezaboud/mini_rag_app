d76756b4bf74fa6b1088a1026a1fec268f6cb356
ramezaboud <ramezaboud248@gmail.com>
2026-03-02 22:10:47 +0200

    Refactor startup and shutdown event handling for database connection and LLM client initialization

--- src/main.py
+++ src/main.py
@@ -2,18 +2,36 @@ from fastapi import FastAPI
 from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
 app = FastAPI()
 
-@app.on_event("startup")
+
 async def startup_db_client():
+
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
-@app.on_event("shutdown")
+    llm_provider_factory = LLMProviderFactory(config=settings)
+
+    # for generation client
+    app.generation_client = llm_provider_factory.create(settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
+
+    # for embedding client
+    app.embedding_client = llm_provider_factory.create(settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
+                                             embedding_size=settings.EMBEDDING_SIZE)
+    
+
+
 async def shutdown_db_client():
     app.mongo_conn.close()    
+
+
+app.router.lifespan.append(startup_db_client)
+app.router.lifespan.append(shutdown_db_client)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
